a5ffcb39cbc7d652dad489c642687bcf6901d2d7
rostyslav48 <rostyslavkravchuk05@gmail.com>
2023-11-25 21:59:52 +0200

    add button to open the widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleWidgetOpen = async () => {
+    const widget: any = document.querySelector("tint-vto");
+    if (widget) {
+      await widget.open();
+
+      await widget.useWebcam().then((wcam: any) => {
+        if (!wcam)
+          fetch("model.webp")
+            .then((r) => r.blob())
+            .then((b) => widget.usePhoto(b));
+      });
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <button onClick={handleWidgetOpen} type="button">
+      Open widget
+    </button>
+  );
 }
 
-export default App
+export default App;
